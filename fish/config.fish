93b07fa6e1aa2ac687c5a2326376068517912c0c
ala-wsl <ala-wsl@gmail.com>
2024-06-06 12:49:01 +0200

    up

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,7 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.local/share/bob/nvim-bin
 fish_add_path ~/.local/share/nvim/mason/bin
 
-### EXPORT
+### Environment variables
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
@@ -28,17 +28,18 @@ abbr -a v 'nvim'
 abbr -a y 'yazi'
 abbr -a gu 'gitui'
 abbr -a lg 'lazygit'
-alias reload='source ~/.config/fish/config.fish'
-alias start='/mnt/c/Windows/explorer.exe'
+alias reload 'source ~/.config/fish/config.fish'
+alias start '/mnt/c/Windows/explorer.exe'
 
 # confirm before overwriting something
 abbr -a cp "cp -i"
 abbr -a mv "mv -i"
 abbr -a rm "rm -i"
 
-alias l='eza -la -s Name --binary --git --group-directories-first --icons'
-alias ll='eza -l --group-directories-first --icons' # long format
-alias lt='eza -T --group-directories-first --icons' # tree listing
+alias ls eza
+alias l 'eza -la -s Name --binary --git --group-directories-first --icons'
+alias ll 'eza -l --group-directories-first --icons' # long format
+alias lt 'eza -T --group-directories-first --icons' # tree listing
  
 # Add this to the end of your config file
 zoxide init fish | source
