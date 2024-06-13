c92ea16d68a4b5edd1f5b5e59cdd64ce2a288a30
ala-wsl <ala-wsl@gmail.com>
2024-06-13 19:39:09 +0200

    fish: yy function to change the current working directory when exiting Yazi

--- fish/config.fish
+++ fish/config.fish
@@ -18,18 +18,18 @@ set -x XDG_STATE_HOME $HOME/.local/state
 set -x ZDOTDIR $XDG_CONFIG_HOME/zsh
 
 set fish_greeting # Supresses fish's intro message
-set -x EDITOR 'hx'
+set -x EDITOR hx
 
 set -x MOAR '--no-statusbar --style=github-dark'
 set -x PAGER 'moar --no-linenumbers -quit-if-one-screen'
 # bat as manpager
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-set -x MANROFFOPT '-c'
+set -x MANROFFOPT -c
 
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 
 # bun
@@ -37,12 +37,13 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 ### aliases
-abbr -a v 'nvim'
-abbr -a y 'yazi'
-abbr -a gu 'gitui'
-abbr -a lg 'lazygit'
+abbr -a v nvim
+abbr -a y yazi
+abbr -a gu gitui
+abbr -a lg lazygit
 alias reload 'source ~/.config/fish/config.fish'
 alias start '/mnt/c/Windows/explorer.exe'
+alias gr 'cd (git rev-parse --show-toplevel)'
 
 # confirm before overwriting something
 abbr -a cp "cp -i"
@@ -53,6 +54,16 @@ alias ls eza
 alias l 'eza -la -s Name --binary --git --group-directories-first --icons'
 alias ll 'eza -l --group-directories-first --icons'
 alias lt 'eza --tree --group-directories-first --icons'
+
+### functions
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
 
 # Add this to the end of your config file
 zoxide init fish | source
