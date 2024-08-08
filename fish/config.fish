227174b9c13dd3ecc21b7aae1405a4ba0cc1250c
sibouras <sibouras@users.noreply.github.com>
2024-08-08 15:32:25 +0200

    fish: display indicator when inside a yazi subshell

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fish_config prompt choose arrow
+# fish_config prompt choose arrow
 
 ### Adding to path
 fish_add_path ~/.local/bin
