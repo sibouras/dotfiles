bcce1dc10acdba87de7a52f6f40716ff08273cfb
sibouras <sibouras@users.noreply.github.com>
2025-02-03 16:53:08 +0100

    misc

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 
 mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/src"
 mkdir -p "$HOME/.local/share/bash-completion/completions"
 mkdir -p "$HOME/.local/share/zsh-completion"
 mkdir -p "$HOME/.local/state/bash" # for bash history file
