9e219435e62b86c2b6d75114c0f7ee68a1e9b719
sibouras <sibouras@users.noreply.github.com>
2026-02-16 14:11:46 +0100

    yazi: v25.12.29 updates

---
+++ yazi/plugins/zoom-preview.yazi/main.lua
@@ -0,0 +1,28 @@
+--- @since 25.2.26
+--- @sync entry
+
+local function entry(st)
+  if st.old then
+    Tab.layout, st.old = st.old, nil
+  else
+    st.old = Tab.layout
+    Tab.layout = function(self)
+      local r = rt.mgr.ratio
+      self._chunks = ui.Layout()
+        :direction(ui.Layout.HORIZONTAL)
+        :constraints({
+          ui.Constraint.Length(1),
+          ui.Constraint.Ratio(r.parent, r.parent + r.current),
+          ui.Constraint.Ratio(r.current, r.parent + r.current),
+        })
+        :split(self._area)
+    end
+  end
+  ya.emit('app:resize', {})
+end
+
+local function enabled(st)
+  return st.old ~= nil
+end
+
+return { entry = entry, enabled = enabled }
